44e695e8651a1caff64077613534488d43f10bbb
Dphu11701 <dphu569@gmail.com>
2023-12-07 14:28:42 +0700

    aaa

--- config/dbConection.js
+++ config/dbConection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.connection_string);
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
     console.log(
       "Database connected: ",
       connect.connection.host,
